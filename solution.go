304450f198a37f34b1c078464f52842259758577
LXie-Saintex <admin@saintex.co.uk>
2020-06-19 17:21:44 +0100

    removed duplicates

--- solution.go
+++ solution.go
@@ -9,34 +9,44 @@ func Solution() [][]int {
 	var solutions [][]int
 	var a, b int
 	m := make(map[float64][]int)
-	am := make(map[int]int)
 
 	for a = 1; a < 1000; a++ {
 		for b = 1; b < 1000; b++ {
 			sum := PowOfThree(a) + PowOfThree(b)
 			if v, ok := m[sum]; ok == true && a+b != v[0]+v[1] {
-				solutions = append(solutions, []int{a, b, v[0], v[1]})
+				if set := []int{a, b, v[0], v[1]}; found(set, solutions) == false {
+					solutions = append(solutions, set)
+				}
+
 			} else {
 				m[sum] = []int{a, b}
 			}
 		}
 	}
-	for i, s := range solutions {
-		sum := 0
-		for _, num := range s {
-			sum += num
-		}
-		if _, ok := am[sum]; ok == true {
-			solutions = append(solutions[:i], solutions[i+1:]...)
-		} else {
-			am[sum] = sum
-		}
-	}
-
 	return solutions
 }
 
 //PowOfThree returns the number to the power of 3
 func PowOfThree(num int) float64 {
 	return math.Pow(float64(num), 3)
+}
+
+func found(set []int, total [][]int) bool {
+	var result bool
+	setsum := 0
+
+	for _, y := range set {
+		setsum += y
+	}
+
+	for _, s := range total {
+		sum := 0
+		for _, x := range s {
+			sum += x
+		}
+		if sum == setsum {
+			result = true
+		}
+	}
+	return result
 }
