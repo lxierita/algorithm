6f11ecb94a8726cfb77eef3a218717990910d5b6
LXie-Saintex <admin@saintex.co.uk>
2020-08-16 11:34:07 +0100

    server to keep container running

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	s "algorithm/solution"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
 	if res := s.Solution(); res != nil {
-		fmt.Println(res)
+		fmt.Fprintf(w, "result= %v\n", res)
 	}
 }
